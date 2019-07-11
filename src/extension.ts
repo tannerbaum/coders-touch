14c830670a1578cd5a5119bc963772ed8cd6da41
tannerbaum <tanner@Tanners-MBP.fritz.box>
2019-07-11 17:01:28 +0200

    remove disposable code

--- src/extension.ts
+++ src/extension.ts
@@ -1,53 +1,45 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	let disposable;
-	
-	disposable = vscode.commands.registerTextEditorCommand('extension.leftBracket', () => {
-		const editor = vscode.window.activeTextEditor;
+    vscode.commands.registerTextEditorCommand("extension.leftBracket", () => {
+        const editor = vscode.window.activeTextEditor;
 
-		if(editor){
-			const selection = editor.selection;
+        if (editor) {
+            const selection = editor.selection;
 
-			editor.edit(editBuilder => {
-				editBuilder.insert(selection.start, "{")
-			});
-		}
-	});
+            editor.edit(editBuilder => {
+                editBuilder.insert(selection.start, "{");
+            });
+        }
+    });
 
-	context.subscriptions.push(disposable);
+    vscode.commands.registerTextEditorCommand("extension.rightBracket", () => {
+        const editor = vscode.window.activeTextEditor;
 
-	disposable = vscode.commands.registerTextEditorCommand('extension.rightBracket', () => {
-		const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const selection = editor.selection;
 
-		if(editor){
-			const selection = editor.selection;
+            editor.edit(editBuilder => {
+                editBuilder.insert(selection.start, "}");
+            });
+        }
+    });
 
-			editor.edit(editBuilder => {
-				editBuilder.insert(selection.start, "}")
-			});
-		}
-	});
+    vscode.commands.registerTextEditorCommand("extension.arrowFunction", () => {
+        const editor = vscode.window.activeTextEditor;
 
-	context.subscriptions.push(disposable);
+        if (editor) {
+            const selection = editor.selection;
 
-	disposable = vscode.commands.registerTextEditorCommand('extension.arrowFunction', () => {
-		const editor = vscode.window.activeTextEditor;
-
-		if(editor){
-			const selection = editor.selection;
-
-			editor.edit(editBuilder => {
-				editBuilder.insert(selection.start, "() => { }")
-			});
-		}
-	});
-
-	context.subscriptions.push(disposable);
+            editor.edit(editBuilder => {
+                editBuilder.insert(selection.start, "() => { }");
+            });
+        }
+    });
 }
 
 // this method is called when your extension is deactivated
